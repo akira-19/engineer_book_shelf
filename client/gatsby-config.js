e44a9eedd9969d2688b58466a15124bb6c221521
Akira T <akira.tsuruoka.19@gmail.com>
2020-12-13 17:56:11 +0900

    add graphql

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -23,5 +23,27 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "bookshelf",
+        // This is the field under which it's accessible
+        fieldName: "bookshelf",
+        // URL to query from
+        url: "http://localhost:8080/query",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-graphql-codegen",
+      options: {
+        fileName: `types/graphql-types.d.ts`,
+        documentPaths: [
+          './src/**/*.{ts,tsx}',
+          './node_modules/gatsby-*/**/*.js',
+          './gatsby-node.ts',
+        ],
+      },
+    },
   ],
 };
